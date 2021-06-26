475bab2d45aac97e68aac0ec535e3da7769efb46
steven <sjehann@gmail.com>
2021-06-27 00:40:49 +0200

    trying redis and add reorder admin button

--- src/PezosSandbox/Infrastructure/Symfony/Controller/MemberArea/TokenController.php
+++ src/PezosSandbox/Infrastructure/Symfony/Controller/MemberArea/TokenController.php
@@ -10,7 +10,11 @@ use PezosSandbox\Application\FlashType;
 use PezosSandbox\Application\UpdateToken;
 use PezosSandbox\Domain\Model\Common\UserFacingError;
 use PezosSandbox\Domain\Model\Token\CouldNotFindToken;
+use PezosSandbox\Infrastructure\Mapping;
 use PezosSandbox\Infrastructure\Symfony\Form\TokenForm;
+use PezosSandbox\Infrastructure\Tezos\Contract;
+use PezosSandbox\Infrastructure\Tezos\Decimals;
+use PezosSandbox\Infrastructure\Tezos\StorageHistory\GetStorageHistory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,15 +27,20 @@ use Symfony\Contracts\Translation\TranslatorInterface;
  */
 final class TokenController extends AbstractController
 {
+    use Mapping;
+
     private ApplicationInterface $application;
     private TranslatorInterface $translator;
+    private GetStorageHistory $getStorageHistory;
 
     public function __construct(
         ApplicationInterface $application,
-        TranslatorInterface $translator
+        TranslatorInterface $translator,
+        GetStorageHistory $getStorageHistory
     ) {
-        $this->application = $application;
-        $this->translator  = $translator;
+        $this->application       = $application;
+        $this->translator        = $translator;
+        $this->getStorageHistory = $getStorageHistory;
     }
 
     /**
@@ -139,6 +148,55 @@ final class TokenController extends AbstractController
         );
 
         $this->application->updateToken($updateToken);
+
+        return $this->redirectToRoute('app_token_list');
+    }
+
+    /**
+     * @Route("/tokens/reorder", name="app_token_reorder", methods={"POST"})
+     */
+    public function reorder(Request $request): Response
+    {
+        $tokens   = $this->application->listTokens();
+        $tezPools = [];
+
+        foreach ($tokens as $t) {
+            $token = $this->application->getOneTokenByAddress(
+                $t->address()->asString()
+            );
+            $tezPools[$token->address()->asString()] = 0;
+            if ($token->isActive()) {
+                $history = $this->getStorageHistory
+                    ->getStorageHistory(
+                        Contract::fromString(
+                            $token->exchanges()[0]->contract()
+                        ),
+                        Decimals::fromInt($token->metadata()['decimals'])
+                    )
+                    ->history($this->application->getCurrentTime());
+                $history                                 = end($history);
+                $tezPools[$token->address()->asString()] = self::asInt(
+                    $history,
+                    'tez_pool'
+                );
+            }
+        }
+
+        arsort($tezPools);
+        $tezPools = array_flip($tezPools);
+        $position = 0;
+        foreach ($tezPools as $address) {
+            $position    = $position + 1;
+            $token       = $this->application->getOneTokenByAddress($address);
+            $updateToken = new UpdateToken(
+                $token->tokenId()->asString(),
+                $token->address()->asString(),
+                $token->metadata(),
+                $token->isActive(),
+                $position
+            );
+            $this->application->updateToken($updateToken);
+        }
 
         return $this->redirectToRoute('app_token_list');
     }
