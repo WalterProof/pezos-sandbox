badce547e7ef98ffc3c8fcc605073c011e1d9081
steven <sjehann@gmail.com>
2021-09-05 22:10:43 +0200

    cleanup

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,26 +5,33 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Http\TezTools\Client as TezTools;
+use App\Model\Contract;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public const DEFAULT_TOKEN_IDENTIFIER = 'KT1GRSvLoikDsXujKgZPsGLX8k8VvR2Tq95b';
+
     public function __construct(
         private TezTools $teztools
     ) {
     }
 
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $tokens       = $this->teztools->fetchContracts();
-        $currentToken = array_pop($tokens);
+        $identifier = $request->query->get('identifier', self::DEFAULT_TOKEN_IDENTIFIER);
+
+        $tokens        = $this->teztools->fetchContracts();
+        $filtered      = array_filter($tokens, fn (Contract $contract): bool => $contract->identifier === $identifier);
+        $selectedToken = array_pop($filtered);
 
         return $this->render('homepage.html.twig', [
-            'tokens'       => $tokens,
-            'currentToken' => $currentToken,
+            'tokens'        => $tokens,
+            'selectedToken' => $selectedToken,
         ]);
     }
 }
--- src/Model/Contract.php
+++ src/Model/Contract.php
@@ -6,10 +6,13 @@ namespace App\Model;
 
 class Contract
 {
+    public string $identifier;
     public string $symbol;
     public string $tokenAddress;
     public int $tokenId;
     public string $name;
     public string $type;
     public string $address;
+    public string $thumbnailUri;
+    public string $tags;
 }
