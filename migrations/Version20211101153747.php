668f6ed892b54c99b60fdeb1e46e61185a894f06
bzzhh <sjehann@gmail.com>
2021-11-02 00:06:12 +0100

    remove uneeded migration

--- migrations/Version20211101153747.php
+++ migrations/Version20211101153747.php
@@ -23,18 +23,6 @@ final class Version20211101153747 extends AbstractMigration
         $this->addSql('CREATE SEQUENCE contract_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE contract (id INT NOT NULL, symbol VARCHAR(255) NOT NULL, identifier VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, decimals INT NOT NULL, total_supply VARCHAR(255) NOT NULL, thumbnail_uri VARCHAR(255) DEFAULT NULL, website_link VARCHAR(255) DEFAULT NULL, telegram_link VARCHAR(255) DEFAULT NULL, twitter_link VARCHAR(255) DEFAULT NULL, discord_link VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE price_history (id SERIAL NOT NULL, token VARCHAR(255) NOT NULL, timestamp TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, price NUMERIC(27, 18) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
-        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
-        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
-        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
-            BEGIN
-                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
-                RETURN NEW;
-            END;
-        $$ LANGUAGE plpgsql;');
-        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
-        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
     }
 
     public function down(Schema $schema): void
@@ -44,6 +32,5 @@ final class Version20211101153747 extends AbstractMigration
         $this->addSql('DROP SEQUENCE contract_id_seq CASCADE');
         $this->addSql('DROP TABLE contract');
         $this->addSql('DROP TABLE price_history');
-        $this->addSql('DROP TABLE messenger_messages');
     }
 }
